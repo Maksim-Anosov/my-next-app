db21aa5bc0ccf1feb524bd383ae4f807a82e46b6
Maksim Anosov <eastcoastcustoms@yandex.ru>
2024-11-30 13:00:56 +0300

    CompoundComponent

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,77 @@
-import { Metadata } from "next";
+'use client';
+// import { Metadata } from 'next';
+import { createContext, useCallback, useContext, useState } from 'react';
 
-export const metadata: Metadata = {
-  title: 'About',
-};
+// export const metadata: Metadata = {
+//   title: 'About'
+// };
 
 export default function About() {
+  return <CompoundComponent />;
+}
+
+interface IMenuContext {
+  activeGroup: string;
+  switchGroup: (title: string) => void;
+}
+
+const MenuContext = createContext<IMenuContext>({} as IMenuContext);
+
+const MenuAccordion = ({ children }: { children: React.ReactNode }) => {
+  const [activeGroup, setActiveGroup] = useState("");
+
+  const switchGroup = useCallback((title: string) => {
+    setActiveGroup(activeTitle => activeTitle === title ? "" : title);
+  }, []);
+
+  return <MenuContext.Provider value={{ activeGroup, switchGroup }}>
+    <div style={{display: 'flex', justifyContent: 'space-around'}}>{children}</div>
+    </MenuContext.Provider>;
+};
+
+type MenuGroupProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+MenuAccordion.Group = function MenuGroup({ title, children }: MenuGroupProps) {
+  const { activeGroup, switchGroup } = useContext(MenuContext);
   return (
-    <h1 style={{ textAlign: 'center', fontSize: '50px', height: '80vh' }}>
-      About
-    </h1>
+    <div>
+      <button style={{ width: '200px' }} onClick={() => switchGroup(title)}>{title}</button>
+      {activeGroup === title && <div>{children}</div>}
+    </div>
+  );
+};
+
+MenuAccordion.Item = function MenuItem({ title }: { title: string }) {
+  return (
+    <div>
+      <div>{title}</div>
+    </div>
+  );
+};
+
+function CompoundComponent() {
+  return (
+    <div style={{ textAlign: 'center', fontSize: '50px', height: '80vh' }}>
+      <MenuAccordion>
+        <MenuAccordion.Group title='Меню'>
+          <MenuAccordion.Item title='Пункт 1' />
+          <MenuAccordion.Item title='Пункт 2' />
+          <MenuAccordion.Item title='Пункт 3' />
+        </MenuAccordion.Group>
+        <MenuAccordion.Group title='О нас'>
+          <MenuAccordion.Item title='Пункт 1' />
+          <MenuAccordion.Item title='Пункт 2' />
+          <MenuAccordion.Item title='Пункт 3' />
+        </MenuAccordion.Group>
+        <MenuAccordion.Group title='Служебное'>
+          <MenuAccordion.Item title='Пункт 1' />
+          <MenuAccordion.Item title='Пункт 2' />
+          <MenuAccordion.Item title='Пункт 3' />
+        </MenuAccordion.Group>
+      </MenuAccordion>
+    </div>
   );
 }
--- src/app/components/PostsList.tsx
+++ src/app/components/PostsList.tsx
@@ -1,27 +1,31 @@
 'use client';
-import { useEffect } from "react";
-import { useStore } from "../store/store";
-import s from './style.module.css'
-import Link from "next/link";
-import Loading from "../blog/loading";
-import { useShallow } from "zustand/shallow";
+import { useEffect } from 'react';
+import { useStore } from '../store/store';
+import s from './style.module.css';
+import Link from 'next/link';
+import Loading from '../blog/loading';
+import { useShallow } from 'zustand/shallow';
 
 export function PostsList() {
-  const [posts, loading, getPosts] = useStore(useShallow((state) => [
-    state.posts,
-    state.loading,
-    state.getPosts
-  ]));
+  const [posts, loading, getPosts] = useStore(
+    useShallow((state) => [state.posts, state.loading, state.getPosts])
+  );
 
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return (
-    loading ? <Loading /> : <ul className={s.blog}>
+  return loading ? (
+    <Loading />
+  ) : (
+    <ul className={s.blog}>
       {posts.map((post) => (
-        <li key={post.id}><Link className={s.link} href={`/blog/${post.id}`}>{post.title}</Link></li>
+        <li key={post.id}>
+          <Link className={s.link} href={`/blog/${post.id}`}>
+            {post.title}
+          </Link>
+        </li>
       ))}
-    </ul> 
-  )
+    </ul>
+  );
 }
