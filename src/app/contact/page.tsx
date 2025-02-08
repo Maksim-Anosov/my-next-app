fdaf55881b403dfeca2ac425af2051d90cb9b9c9
Maksim Anosov <eastcoastcustoms@yandex.ru>
2025-02-08 12:06:32 +0300

    search debounce with abortController

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,101 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+
+interface UserData {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+}
+
 export default function Contacts() {
+  const [usersData, setUsersData] = useState<UserData[]>([]);
+  const [searchText, setSearchText] = useState('');
+  const [filteredUser, setFilteredUser] = useState<Pick<UserData, 'name'>>();
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('fetched data', data);
+        setUsersData(data);
+      })
+      .catch((error) => {
+        console.log('Errror While fetching user data', error.message);
+      });
+  }, []);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const filterTimer = setTimeout(() => {
+      try {
+        fetch(
+          `https://jsonplaceholder.typicode.com/users?username=${searchText}`,
+          {
+            signal: abortController.signal
+          }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            console.log('FilteredUser ', data);
+            setFilteredUser(data[0]);
+          });
+      } catch (error: unknown) {
+        if (error instanceof Error && error.name === 'AbortError') {
+          console.log('Request was aborted');
+        } else {
+          console.error('Error fetching user data:', error);
+        }
+      }
+    }, 300);
+
+    return () => {
+      abortController.abort();
+      clearTimeout(filterTimer);
+    };
+  }, [searchText]);
+
   return (
-    <h1 style={{ textAlign: 'center', fontSize: '50px', height: '80vh' }}>
-      Contacts
-    </h1>
+    <div style={{ textAlign: 'center' }}>
+      <h4>Users Data</h4>
+      <input
+        type='text'
+        placeholder='Search by username'
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+
+      <table
+        style={{
+          borderCollapse: 'collapse',
+          margin: 'auto',
+          border: '1px solid rgb(0, 233, 245)'
+        }}
+      >
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {usersData &&
+            usersData.map((user) => (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+
+      <div>
+        <h4>Search Result</h4>
+        {filteredUser && filteredUser.name}
+      </div>
+    </div>
   );
 }
